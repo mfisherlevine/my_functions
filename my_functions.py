4680524b6e6b5a5d382179b02d236d2c56b389bb
mmmerlin <mmmerlin@ubuntu.(none)>
2015-04-03 22:24:17 +0100

    03/04/2015 4

--- my_functions.py
+++ my_functions.py
@@ -240,7 +240,7 @@ def GetRawTimecodes_SingleFile(filename, winow_xmin = 0, winow_xmax = 999, winow
 
     return timecodes
 
-def GetTimecodes_AllFilesInDir(path, winow_xmin = 0, winow_xmax = 999, winow_ymin = 0, winow_ymax = 999, offset_us = 0):
+def GetTimecodes_AllFilesInDir(path, winow_xmin = 0, winow_xmax = 999, winow_ymin = 0, winow_ymax = 999, offset_us = 0, translate_to_us = False, glitch_threshold = 20000):
     import string, os
     
     timecodes = []
@@ -248,32 +248,35 @@ def GetTimecodes_AllFilesInDir(path, winow_xmin = 0, winow_xmax = 999, winow_ymi
     
     for filename in os.listdir(path):
         files.append(path + filename)
+        
 
     nfiles = 0
     for filename in files:
         datafile = open(filename)
         nfiles += 1
+        if len(files)>500 and (nfiles % 500 == 0): print 'Loaded %s of %s files'%(nfiles, len(files))
         lines = datafile.readlines()
         
-#         if len(lines) > 50 and len(lines) < 1000:
-#             OpenTimepixInDS9(filename)
-#             exit()
-        
-#         if len(lines) > 1000: continue #skip bad files (glitches)
+        if len(lines) > glitch_threshold: continue #skip files which glitched (most pixels hit - parameter may need tuning)
         for line in lines:
-#             x,y,timecode,dummy1, dummy2 = string.split(str(line),'\t')
             x,y,timecode = string.split(str(line),'\t')
             x = int(x)
             y = int(y)
             timecode = int(timecode)
+            if timecode == 11810: continue  #discard overflows
+            if timecode == 1: continue      #discard noise hits
             if x>=winow_xmin and x<=winow_xmax and y>=winow_ymin and y<=winow_ymax:
-                actual_offset_us = intrinsic_offset - offset_us
-                time_s = (11810. - timecode) * 20e-9
-                time_us = (time_s *1e6)- actual_offset_us
-                timecodes.append(timecode)
-#                 timecodes.append(time_us)
+                if translate_to_us == True:
+                    actual_offset_us = 250 - offset_us
+                    time_s = (11810. - timecode) * 20e-9
+                    time_us = (time_s *1e6)- actual_offset_us
+                    timecodes.append(time_us)
+                else:
+                    timecodes.append(timecode)
+
     print "Loaded data from %s files"%nfiles
     return timecodes
+
 
 def GetXYTarray_AllFilesInDir(path, winow_xmin = 0, winow_xmax = 999, winow_ymin = 0, winow_ymax = 999, offset_us = 0, tmin_us = -1000, tmax_us = 999999, maxfiles = None):
     import string, os
@@ -679,3 +682,40 @@ def GetXYTarray_AllFilesInDir_Raw_Timecodes(path, winow_xmin = 0, winow_xmax = 9
             return xs, ys, ts
 
     return xs, ys, ts  
+
+
+
+def MakeToFSpectrum(input_path, save_path, xmin=0, xmax=255, ymin=0, ymax=255, translate_to_us = True, time_zoom_region = [0,11810]):
+    import pylab as pl
+    
+    timecodes = GetTimecodes_AllFilesInDir(input_path, xmin, xmax, ymin, ymax, 110, translate_to_us)
+    print 'Total number of timecodes read in = %s' %len(timecodes)
+     
+    if translate_to_us:
+        tmin = 5
+        tmax = 25
+        bins = (((tmax-tmin))*50) - 1 #1 timecode per bin
+    else:
+        tmin = 0         # full range
+        tmax = 11810     # full range
+        bins = (tmax-tmin) +1 
+     
+    fig = pl.figure()
+    
+    pl.hist(timecodes, bins = bins, range = [tmin,tmax]) #make the histogram of the timecodes
+    
+#     pl.ylim([0,2000]) #for clipping the y-axis
+    
+    if translate_to_us:
+        pl.xlabel('ToF (us)', horizontalalignment = 'right' )
+    else:
+        pl.xlabel('Timecodes', horizontalalignment = 'right' )
+    
+    pl.title('Timepix ToF Spectrum')
+    fig.savefig(save_path + '_ToF_Full.png')
+
+    tmin, tmax = time_zoom_region
+    pl.xlim([tmin,tmax]) #for clipping the x-axis
+    fig.savefig(save_path + '_ToF_ROI.png')
+    
+
