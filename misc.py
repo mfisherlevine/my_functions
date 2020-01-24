01c8e8edc62336cc236c23851ca1d24d10e52c0d
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2020-01-23 18:01:19 -0600

    Start file for misc stuff

---
+++ misc.py
@@ -0,0 +1,6 @@
+
+def retrieve_name(var):
+    import inspect
+
+    callers_local_vars = inspect.currentframe().f_back.f_locals.items()
+    return [var_name for var_name, var_val in callers_local_vars if var_val is var][0]
