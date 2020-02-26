f29332ab2cb4687d001a0e52cea9e340dab6c68a
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2020-02-26 15:20:15 -0600

    Add noglobals decorator

--- misc.py
+++ misc.py
@@ -1,3 +1,24 @@
+import types
+
+
+def imports():
+    for name, val in globals().items():
+        # module imports
+        if isinstance(val, types.ModuleType):
+            yield name, val
+        # functions / callables
+        if hasattr(val, '__call__'):
+            yield name, val
+
+
+noglobal = lambda fn: types.FunctionType(fn.__code__, dict(imports()))
+
+# use as:
+# a = 1
+# @noglobal
+# def f(b):
+#     print(a)
+
 
 def retrieve_name(var):
     import inspect
