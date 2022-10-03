b9ddf7a8233b30b257965a1f57591b5ead6cec57
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2022-10-03 10:47:23 -0700

    WIP

--- jira_it.py
+++ jira_it.py
@@ -5,6 +5,8 @@ import requests as rq
 import json
 import argparse
 from bs4 import BeautifulSoup
+import os
+import subprocess
 
 def check_url(url=None):
     if url is not None:
@@ -12,7 +14,7 @@ def check_url(url=None):
         R = rq.get(waybackUrl)
         BS = BeautifulSoup(R.text, 'lxml')
         #        print(R.content)
- 
+
         tmp = BS.find_all("h1", {'id': 'summary-val'})
         summary = tmp[0].text.rstrip().lstrip()
         tmp = BS.find_all("span", {'id': 'status-val'})
@@ -34,6 +36,12 @@ parser.add_argument("urls", nargs='*')
 parser.add_argument("-v", "--verbose", action='store_true', default=False)
 args = parser.parse_args()
 
+if not args.urls:  # no ticket specified so find out the current dir's ticket
+    path = os.getcwd()
+    branchCommand = f"git --git-dir={path}/.git --work-tree={path} symbolic-ref --short HEAD"
+    branchName = subprocess.check_output(branchCommand.split(), universal_newlines=True)
+    args.urls = branchName
+
 for u in args.urls:
     if 'DM-' in u:
         u = re.sub('^.*/', '', u)
@@ -44,4 +52,3 @@ for u in args.urls:
             print(f"{u}  {ss} <{st}>@{ass} RR:{rev}")
         if args.verbose:
             print(f"         {fullUrl}")
-
